2b26a3a7823a49028beb54fdc8c3267092cf1fb2
mimxu <leemx-wm19@student.tarc.edu.my>
2022-04-16 17:06:12 +0800

    kotlin ggd

--- app/src/main/java/com/example/mobileassignment/ProfileFragment.kt
+++ app/src/main/java/com/example/mobileassignment/ProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 import com.example.mobileassignment.R
 
 
@@ -22,8 +24,12 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        val gotoEdit: Button = view.findViewById(R.id.editProfile)
+        gotoEdit.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_nav_profile_to_editProfile2)
+        }
+        return view
     }
 
 
