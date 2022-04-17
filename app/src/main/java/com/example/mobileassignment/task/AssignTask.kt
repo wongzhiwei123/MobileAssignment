aaf292993ff3fae09ca33f66ae99cd0a5534b283
Seng Tian <jacktst01@gmail.com>
2022-04-17 22:53:32 +0800

    Life is beautiful

--- app/src/main/java/com/example/mobileassignment/task/AssignTask.kt
+++ app/src/main/java/com/example/mobileassignment/task/AssignTask.kt
@@ -52,9 +52,13 @@ class AssignTask : Fragment() {
                 training.text.clear()
                 desc.text.clear()
                 set.text.clear()
+
+
                     Navigation.findNavController(view).navigate(R.id.action_nav_assignTask_to_tasklist)
 
                 Toast.makeText(context, "Successfully uploaded", Toast.LENGTH_SHORT).show()
+
+
                 }.addOnFailureListener{
                     Toast.makeText(context,"Failed",Toast.LENGTH_SHORT).show()
                 }
