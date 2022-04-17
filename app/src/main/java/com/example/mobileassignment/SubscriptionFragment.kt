7b3ab65cca795bb524ba6b22101184718591215a
wongz <wongzhiwei123>
2022-04-17 16:23:42 +0800

    John 8.0

--- app/src/main/java/com/example/mobileassignment/SubscriptionFragment.kt
+++ app/src/main/java/com/example/mobileassignment/SubscriptionFragment.kt
@@ -1,14 +1,24 @@
 package com.example.mobileassignment
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-import com.example.mobileassignment.R
+import androidx.navigation.fragment.findNavController
 import com.example.mobileassignment.subscription.CheckoutFragment
+import com.example.mobileassignment.subscription.PaymentMethodFragment
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,7 +27,8 @@ import com.example.mobileassignment.subscription.CheckoutFragment
  */
 class SubscriptionFragment : Fragment() {
 
-
+    private var param1: String? = null
+    private var param2: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -31,17 +42,58 @@ class SubscriptionFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_subscription, container, false)
-        val select_basic: Button = view.findViewById(R.id.btn_basic)
-        select_basic.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_nav_subscription_to_checkoutFragment)
+
+        val subscribed1:ImageView=view.findViewById(R.id.basicSubscribed)
+        val subscribed2:ImageView=view.findViewById(R.id.premiumSubscribed)
+        subscribed1.isVisible=false
+        subscribed2.isVisible=false
+        val btn1:Button=view.findViewById(R.id.btn_basic)
+        val btn2:Button=view.findViewById(R.id.btn_premium)
+        // Read from the database
+        val database = Firebase.database("https://findyourcoach-3083a-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        val myRef = database.getReference("user")
+        val test: TextView = view.findViewById(R.id.textView5)
+        myRef.child("account1").child("plan").get().addOnSuccessListener {
+            val type = it.value.toString()
+            test.text=type
+            if(type=="Basic"){
+                btn1.isVisible=false
+                subscribed1.isVisible=true
+                btn2.isVisible=true
+                subscribed2.isVisible=false
+            }else if(type=="Premium"){
+                btn1.isVisible=false
+                subscribed1.isVisible=false
+                btn2.isVisible=false
+                subscribed2.isVisible=true
+            }
+        }.addOnFailureListener{
+            //Log.e("firebase", "Error getting data", it)
         }
 
-        val select_premium: Button = view.findViewById(R.id.btn_premium)
-        select_premium.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_nav_subscription_to_addBasisClass)
+        val selectBasic: Button = view.findViewById(R.id.btn_basic)
+        var selectedPlan = 0
+        selectBasic.setOnClickListener{
+            selectedPlan = 1
+            findNavController().navigate(R.id.action_nav_subscription_to_checkoutFragment, Bundle().apply {
+                putInt("itemID", selectedPlan)
+            })
+            //Navigation.findNavController(view).navigate(R.id.action_nav_subscription_to_checkoutFragment)
+
         }
+
+        val selectPremium: Button = view.findViewById(R.id.btn_premium)
+        selectPremium.setOnClickListener{
+            selectedPlan = 2
+            findNavController().navigate(R.id.action_nav_subscription_to_checkoutFragment, Bundle().apply {
+                putInt("itemID", selectedPlan)
+            })
+//            Navigation.findNavController(view).navigate(R.id.action_nav_subscription_to_checkoutFragment)
+//            selectedPlan = 2
+        }
+
+
         return view
     }
-
 
 }
